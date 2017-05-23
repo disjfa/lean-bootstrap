b3deb2bd09563dc1d5d2b75c202b2d62cf155a73
disjfa <disjfa@disjfa.nl>
2017-05-23 22:31:58 +0000

    save unasigned projects

--- server/routes/projects.js
+++ server/routes/projects.js
@@ -1,31 +1,31 @@
-const router = require('express').Router();
-const sass = require('node-sass');
-const fs = require('fs');
-const path = require('path');
-const parseData = require('../modules/projects/parser');
-const projectData = require('../modules/projects/project');
-const uuid = require('uuid/v4');
+const router = require('express').Router()
+const sass = require('node-sass')
+const fs = require('fs')
+const path = require('path')
+const parseData = require('../modules/projects/parser')
+const projectData = require('../modules/projects/project')
+const uuid = require('uuid/v4')
 
 router.get('/', (req, res) => {
-  const projects = req.database.getCollection('projects');
-  res.send(projects.data);
-});
+  const projects = req.database.getCollection('projects')
+  res.send(projects.data)
+})
 
 router.get('/my', (req, res) => {
   if (!req.user) {
-    return res.status(403).send({ data: {}, statusText: 'not allowed', code: 403 });
+    return res.status(403).send({ data: {}, statusText: 'not allowed', code: 403 })
   }
-  const projects = req.database.getCollection('projects');
-  const myProjects = projects.find({ userId: req.user.uuid });
-  res.send(myProjects);
-});
+  const projects = req.database.getCollection('projects')
+  const myProjects = projects.find({ userId: req.user.uuid })
+  res.send(myProjects)
+})
 
 router.post('/', (req, res) => {
-  const projects = req.database.getCollection('projects');
+  const projects = req.database.getCollection('projects')
 
-  let userId = null;
+  let userId = null
   if (req.user) {
-    userId = req.user.uuid;
+    userId = req.user.uuid
   }
 
   if (req.body.name) {
@@ -33,49 +33,49 @@ router.post('/', (req, res) => {
       uuid: uuid(),
       name: req.body.name,
       userId,
-    });
+    })
 
     parseData.parseFile(project).then((varData) => {
       res.send({
         project,
         varData,
-      });
-    });
+      })
+    })
   }
-});
+})
 
 router.get('/:uuid', (req, res) => {
-  const projects = req.database.getCollection('projects');
-  const project = projects.findOne({ uuid: req.params.uuid });
+  const projects = req.database.getCollection('projects')
+  const project = projects.findOne({ uuid: req.params.uuid })
 
   if (!project) {
-    res.status(404).send();
+    res.status(404).send()
   }
 
   parseData.parseFile(project).then((varData) => {
     res.send({
       project,
       varData,
-    });
-  });
-});
+    })
+  })
+})
 
 router.get('/:uuid/:page', (req, res) => {
-  const projects = req.database.getCollection('projects');
-  const project = projects.findOne({ uuid: req.params.uuid });
+  const projects = req.database.getCollection('projects')
+  const project = projects.findOne({ uuid: req.params.uuid })
   if (!project) {
-    return req.next();
+    return req.next()
   }
 
-  const pagename = req.params.page.toLowerCase().replace(/[^a-z0-9]/g, '');
-  let pageslug = 'projects/pages/home';
+  const pagename = req.params.page.toLowerCase().replace(/[^a-z0-9]/g, '')
+  let pageslug = 'projects/pages/home'
   try {
-    const page = fs.lstatSync(`${req.viewsDir}/projects/pages/${pagename}.hbs`);
+    const page = fs.lstatSync(`${req.viewsDir}/projects/pages/${pagename}.hbs`)
     if (page.isFile()) {
-      pageslug = `projects/pages/${pagename}`;
+      pageslug = `projects/pages/${pagename}`
     }
   } catch (err) {
-    return req.next();
+    return req.next()
   }
 
   res.render(pageslug, {
@@ -83,82 +83,85 @@ router.get('/:uuid/:page', (req, res) => {
     title: 'Projects',
     project,
     cssFile: projectData.getCss(req.publicDir, project),
-  });
-});
+  })
+})
 
 router.post('/:uuid/settings', (req, res) => {
-  const projects = req.database.getCollection('projects');
-  const project = projects.findOne({ uuid: req.params.uuid });
+  const projects = req.database.getCollection('projects')
+  const project = projects.findOne({ uuid: req.params.uuid })
   const { user } = req;
+  const userId = user ? user.uuid : null;
+
   if (!project) {
-    res.status(404).send();
+    res.status(404).send()
   }
 
-  if (project.userId && !user || user.uuid !== project.userId) {
-    return res.status(403).send();
+  if (project.userId && userId !== project.userId) {
+    return res.status(403).send()
   }
 
   if (req.body.name) {
-    project.name = req.body.name;
+    project.name = req.body.name
   }
 
   if (req.body.settings instanceof Object) {
-    const settings = {};
+    const settings = {}
     for (const key in req.body.settings) {
       if (!req.body.settings.hasOwnProperty(key)) {
-        continue;
+        continue
       }
-      settings[key] = req.body.settings[key];
+      settings[key] = req.body.settings[key]
     }
-    project.settings = settings;
+    project.settings = settings
   }
-  projects.update(project);
+  projects.update(project)
   res.send({
     message: 'success',
     project,
-  });
-});
+  })
+})
 
 router.post('/:uuid', (req, res) => {
-  const projects = req.database.getCollection('projects');
-  const project = projects.findOne({ uuid: req.params.uuid });
-  const { user } = req;
+  const projects = req.database.getCollection('projects')
+  const project = projects.findOne({ uuid: req.params.uuid })
+  const { user } = req
+  const userId = user ? user.uuid : null;
 
   if (!project) {
-    return res.status(404).send();
+    return res.status(404).send()
   }
 
-  if (project.userId && !user || user.uuid !== project.userId) {
-    return res.status(403).send();
+  if (project.userId && userId !== project.userId) {
+    return res.status(403).send()
   }
 
   parseData.parseFile().then((varData) => {
-    const changed = [];
+    const changed = []
     for (varItem of varData) {
-      const posted = req.body.data[varItem.name];
+      const posted = req.body.data[varItem.name]
       if (posted) {
-        changed.push(`${varItem.name}: ${posted}`);
+        changed.push(`${varItem.name}: ${posted}`)
       }
     }
 
-    const content = project.content;
-    project.content = `${changed.join(';\n')};`;
+    const content = project.content
+    project.content = `${changed.join(';\n')};`
     projectData.render(req.dataDir, req.publicDir, project)
       .then(() => {
-        projects.update(project);
+        projects.update(project)
         res.send({
           message: 'success',
           project,
-        });
+        })
       })
       .catch((error) => {
-        project.content = content;
+        project.content = content
         res.status(400).send({
           message: 'failed',
           error,
-        });
-      });
-  });
-});
+        })
+      })
+  })
+})
 
-module.exports = router;
+module.exports = router
