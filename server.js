c10f2cd9640d96f4960789e3205987188ffcb65b
disjfa <disjfa@disjfa.nl>
2017-09-28 21:34:20 +0000

    Reset server

--- server.js
+++ server.js
@@ -1,117 +1,117 @@
-// to view es6 capabilities see http://node.green/
-// node v8-options es6 module syntax currently under development (2016/06/25)
-const path = require('path');
-const express = require('express');
-const expressHbs = require('express-handlebars');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const loki = require('lokijs');
-const fs = require('fs');
-const routes = require('./server/routes');
-const passport = require('passport');
-const uuid = require('uuid/v4');
+//to view es6 capabilities see http://node.green/
+//node v8-options es6 module syntax currently under development (2016/06/25)
+let path = require('path')
+let express = require('express')
+let expressHbs = require('express-handlebars')
+let cookieParser = require('cookie-parser')
+let bodyParser = require('body-parser')
+let loki = require('lokijs')
+let fs = require('fs')
+let routes = require('./server/routes')
+let passport = require('passport')
+let uuid = require('uuid/v4')
 
-// setup
-const database = new loki('database.loki', {
+//setup
+let database = new loki('database.loki', {
   autoload: true,
   autosave: true,
-});
+})
 if (!database.getCollection('projects')) {
-  database.addCollection('projects');
+  database.addCollection('projects')
 }
 if (!database.getCollection('users')) {
-  database.addCollection('users');
+  database.addCollection('users')
 }
-const app = express();
-// settings
-app.set('port', process.env.PORT || 3000);
-app.set('host', process.env.HOST || 'localhost');
-app.set('env', process.env.NODE_ENV || 'development');
-app.set('views', path.join(__dirname, 'server/views'));
-app.set('dataDir', path.join(__dirname, 'data'));
-app.set('publicDir', path.join(__dirname, 'public/css/data'));
-let config;
-if (fs.existsSync('./config.json') !== false) {
-  config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+let app = express()
+//settings
+app.set('port', process.env.PORT || 3000)
+app.set('host', process.env.HOST || 'localhost')
+app.set('env', process.env.NODE_ENV || 'development')
+app.set('views', path.join(__dirname, 'server/views'))
+app.set('dataDir', path.join(__dirname, 'data'))
+app.set('publicDir', path.join(__dirname, 'public/css/data'))
+let config
+if (false !== fs.existsSync('./config.json')) {
+  config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
 } else {
-  config = {};
+  config = {}
 }
-app.set('auth', config.auth || {});
-app.set('database', database);
+app.set('auth', config.auth || {})
+app.set('database', database)
 
-// view engine & main template
+//view engine & main template
 app.engine('.hbs', expressHbs({
   defaultLayout: 'template',
   extname: '.hbs',
   layoutsDir: 'server/views/layouts/',
   partialsDir: 'server/views/partials/',
-}));
-app.set('view engine', '.hbs');
+}))
+app.set('view engine', '.hbs')
 
-const auth = app.get('auth');
-const { github } = auth;
+const auth = app.get('auth')
+const { github } = auth
 
-passport.serializeUser((user, done) => {
+passport.serializeUser(function(user, done) {
   done(null, user);
 });
 
-passport.deserializeUser((user, done) => {
+passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
 if (github && github.clientID && github.clientSecret) {
-  const GitHubStrategy = require('passport-github2');
+  let GitHubStrategy = require('passport-github2');
   passport.use(new GitHubStrategy({
-    clientID: github.clientID,
-    clientSecret: github.clientSecret,
-    callbackURL: github.callbackURL,
-  },
-    (accessToken, refreshToken, profile, done) => {
-      const users = database.getCollection('users');
-      const user = users.findOne({ githubId: profile.id }) || users.insert({
-        uuid: uuid(),
-        githubId: profile.id,
-        displayName: profile.displayName,
-        username: profile.username,
-      });
-      return done(null, user);
+      clientID: github.clientID,
+      clientSecret: github.clientSecret,
+      callbackURL: github.callbackURL,
     },
-  ));
+    function (accessToken, refreshToken, profile, done) {
+      let users = database.getCollection('users')
+      let user = users.findOne({ githubId: profile.id }) || users.insert({
+          uuid: uuid(),
+          githubId: profile.id,
+          displayName: profile.displayName,
+          username: profile.username,
+        })
+      return done(null, user)
+    }
+  ))
 }
 
-// middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(require('express-session')({
   secret: '46a50826-3b44-11e7-a919-92ebcb67fe33',
   resave: true,
-  saveUninitialized: true,
-}));
+  saveUninitialized: true
+}))
 // use passport session
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/', express.static('public'));
+app.use(passport.initialize())
+app.use(passport.session())
+app.use('/', express.static('public'))
 
-// loki db reference for the router
+//loki db reference for the router
 app.use((req, res, next) => {
-  req.database = database;
-  next();
-});
+  req.database = database
+  next()
+})
 
-// loki db reference for the router
+//loki db reference for the router
 app.use((req, res, next) => {
-  req.viewsDir = app.get('views');
-  req.dataDir = app.get('dataDir');
-  req.publicDir = app.get('publicDir');
-  if (fs.existsSync(req.dataDir) === false) {
-    fs.mkdir(req.dataDir);
+  req.viewsDir = app.get('views')
+  req.dataDir = app.get('dataDir')
+  req.publicDir = app.get('publicDir')
+  if (false === fs.existsSync(req.dataDir)) {
+    fs.mkdir(req.dataDir)
   }
-  next();
-});
+  next()
+})
 
-// router
-routes.create(app);
+//router
+routes.create(app)
 
-// server
-app.listen(app.get('port'), app.get('host'), () => console.log(`Listening on http://${app.get('host')}:${app.get('port')}`));
+//server
+app.listen(app.get('port'), app.get('host'), () => console.log('Listening on http://' + app.get('host') + ':' + app.get('port')))
